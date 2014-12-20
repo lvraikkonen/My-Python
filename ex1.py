6de0c59be038935b85eb250535c8aad6924434d0
Claus Lv <claus.lv@hotmail.com>
2014-12-20 15:38:39 +0800

    start to use Python Notebook

--- ex1.py
+++ ex1.py
@@ -4,12 +4,12 @@ print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
-#print 'I "said" do not touch this.'
+# print 'I "said" do not touch this.'
 
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by python.
 
-print "I could have code like this." # and the comment after is ignored
+print "I could have code like this."  # and the comment after is ignored
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # print "This won't run."
